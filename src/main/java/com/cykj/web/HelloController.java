1748f31dfd1ba5bbae15546d5ee9a1607f12b6a5
zkw9803 <87410834+zkw9803@users.noreply.github.com>
2021-07-22 10:34:56 +0800

    11

--- src/main/java/com/cykj/web/HelloController.java
+++ src/main/java/com/cykj/web/HelloController.java
@@ -26,7 +26,7 @@ private StuServiceImpl stuService;
     @RequestMapping("/hello")
     public @ResponseBody String hello(){
         System.out.println("springMVC我来了");
-        return "HelloWorld zkpw11";
+        return "HelloWorld zkpw1122";
     }
     @RequestMapping("/getLog")
     public @ResponseBody String getLog(String username,String password){
